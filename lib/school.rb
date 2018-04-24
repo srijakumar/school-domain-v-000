07fa4fbc3a9e0478f52799156227d35db9c2e227
Srija S <srijakumar7@gmail.com>
2018-04-24 00:54:34 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -16,13 +16,6 @@ class School
     return roster[grade]
   end
 
-  def sort
-    sorted = {}
-    roster.each do |grade, name|
-      sorted[grade] = name.sort
-    end
-    sorted
 
-  end
 
 end
